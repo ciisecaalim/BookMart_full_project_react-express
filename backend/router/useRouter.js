0588cd18715fd08e0cb52a16be3cb095ddf822cc
Ciise Caalim Ahmed <walalkapo@gmail.com>
2025-10-23 13:56:09 -0700

    Hello

--- backend/router/useRouter.js
+++ backend/router/useRouter.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", userLogin);
-
+router.get('/', async (req,res)=>{
+    res.send('hello')
+})
 module.exports = router;
